41b96a3ffefde6319c58ebc3650ec7b2e9314c60
MinuKumari14 <minuchy1406@gmail.com>
2022-05-18 12:32:12 +0530

    Second Project Uploaded

--- src/package1/TestGitRepo1.java
+++ src/package1/TestGitRepo1.java
@@ -5,6 +5,7 @@ public class TestGitRepo1 {
 	public static void main(String[] args) {
 		System.out.println("Second project uploaded in Git repository");
 		System.out.println("Second project created in git repository");
+		System.out.println("Test");
 	}
 
 }
