18c03300e77aa4a397ef2a5b80543dd11a50e239
MinuKumari14 <minuchy1406@gmail.com>
2022-07-29 08:35:37 +0530

    Main method change

--- src/package1/TestGitRepo1.java
+++ src/package1/TestGitRepo1.java
@@ -4,6 +4,9 @@ public class TestGitRepo1 {
 
 	public static void main(String[] args) {
 		System.out.println("First project uploaded in Git repository");
+		System.out.println("First project uploaded in Git repository.. A");
+		System.out.println("First project uploaded in Git repository.. A");
+		System.out.println("Project upload A");
 	}
 
 }
